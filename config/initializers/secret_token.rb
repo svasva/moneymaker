e22bb041bb218e98d49e649cfb320e5aef9e4038
Vitaly A. Sorokin <erundook@gmail.com>
2012-10-01 22:02:17 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moneymaker::Application.config.secret_token = '238b41324f9f3b1f564b886adc81a2b2c9c2200f07adb818d45f1a9b473d42f59637b314a554c054559fec6e2e9848283765e0043f40d379a34cc8586de93838'
