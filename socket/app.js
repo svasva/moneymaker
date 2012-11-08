c2a5388d0c565b51da7788ed0f75d17b6be457e8
Vitaly A. Sorokin <erundook@gmail.com>
2012-11-08 12:55:01 +0400

    sockserver + issue tracking test #1

--- socket/app.js
+++ socket/app.js
@@ -2,8 +2,22 @@ require('coffee-script');
 
 var express = require('express');
 var app = express();
+app.configure(function(){
+  app.set('port', process.env.PORT || 9999);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.logger('dev'));
+  app.use(express.methodOverride());
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
 var server = require('http').createServer(app);
-server.listen(9999, '0.0.0.0');
-app.use(express.bodyParser());
+server.listen(app.get('port'));
+
 sockets = require('./socket')(app, server);
 require('./routes')(app, sockets);
+
