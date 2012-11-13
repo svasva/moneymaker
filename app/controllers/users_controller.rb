3f8c917da89493b4cf39b6ecb6299752e58d2a42
Vitaly A. Sorokin <erundook@gmail.com>
2012-11-13 19:08:37 +0400

    getItem* commands

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
   def send_message
     @user = User.find params[:id]
+    @args = params[:args]
     case params[:cmd]
     when 'PING'
-      render text: "PONG to #{@user.social}##{@user.social_id}"
+      render json: 'pong'
+    when 'getItem'
+      @item = Item.find(@args.first)
+      render json: @item
+    when 'getItems'
+      @items = Item.all
+      render json: @items
+    else
+      render json: {error: 'command unknown'}
     end
   end
 end
