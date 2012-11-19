e77083cde901d3bdae4e84832b22433b5cc48456
Vitaly A. Sorokin <erundook@gmail.com>
2012-11-19 19:15:20 +0400

    start location

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,6 +11,8 @@ class Item
   field :size_x,            type: Integer, default: 1
   field :size_y,            type: Integer, default: 1
   field :reputation_bonus,  type: Integer, default: 0
+  field :upgrade_id,        type: String
+  field :startup,           type: Boolean, default: false
 
   field :requirements,      type: Hash
   field :rewards,           type: Hash
@@ -18,8 +20,6 @@ class Item
 
   mount_uploader :swf,  SwfUploader
   mount_uploader :icon, SwfUploader
-
-  has_one :upgrade, class_name: 'Item'
 
   validates_presence_of :name, :size_x, :size_y
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,39 +4,41 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :social,            type: String
-  field :social_id,         type: String
-  field :name,              type: String
-  field :male,              type: Boolean
-  field :age,               type: Integer
-  field :last_sign_in,      type: DateTime
-  field :friend_visits,     type: Integer, default: 0
+  field :social,           type: String
+  field :social_id,        type: String
+  field :name,             type: String
+  field :male,             type: Boolean
+  field :age,              type: Integer
+  field :last_sign_in,     type: DateTime
+  field :friend_visits,    type: Integer, default: 0
 
-  field :bank_name,         type: String,  default: 'Мой банк'
-  field :experience,        type: Integer, default: 0
-  field :level,             type: Integer, default: 1
-  field :reputation,        type: Integer, default: 0
-  field :reputation_bonus,  type: Integer, default: 0
-  field :coins,             type: Integer, default: 0
-  field :coins_max,         type: Integer, default: 100
-  field :coins_spent,       type: Integer, default: 0
-  field :money,             type: Integer, default: 0
-  field :money_spent,       type: Integer, default: 0
-  field :money_bought,      type: Integer, default: 0
-  field :credits,           type: Integer, default: 0
-  field :credit_percent,    type: Integer, default: 10
-  field :deposits,          type: Integer, default: 0
-  field :deposit_percent,   type: Integer, default: 10
-  field :client_interval,   type: Integer, default: 10
-  field :crime_interval,    type: Integer, default: 10
+  field :bank_name,        type: String,  default: 'Мой банк'
+  field :experience,       type: Integer, default: 0
+  field :level,            type: Integer, default: 1
+  field :reputation,       type: Integer, default: 0
+  field :reputation_bonus, type: Integer, default: 0
+  field :coins,            type: Integer, default: 0
+  field :coins_max,        type: Integer, default: 100
+  field :coins_spent,      type: Integer, default: 0
+  field :money,            type: Integer, default: 0
+  field :money_spent,      type: Integer, default: 0
+  field :money_bought,     type: Integer, default: 0
+  field :credits,          type: Integer, default: 0
+  field :credit_percent,   type: Integer, default: 10
+  field :deposits,         type: Integer, default: 0
+  field :deposit_percent,  type: Integer, default: 10
+  field :client_interval,  type: Integer, default: 10
+  field :crime_interval,   type: Integer, default: 10
 
   index({social: 1, social_id: 1}, {unique: true})
 
   embeds_many :user_rooms
+  embeds_many :user_items
   embeds_many :user_contracts
-  has_many :user_sockets
+  has_many :user_sockets, dependent: :destroy
   has_and_belongs_to_many :friends, class_name: 'User'
   after_update :update_client
+  after_save :setup_start_location
 
   def update_client
     message = {
@@ -44,6 +46,17 @@ class User
       response: self.changes
     }.to_json
     send_message message
+  end
+
+  def setup_start_location
+    self.user_rooms.destroy_all
+    self.user_contracts.destroy_all
+    Room.where(startup: true).each do |room|
+      UserRoom.create(room: room, user: self)
+    end
+    Item.where(startup: true).each do |item|
+      UserItem.create(user: self, item: item)
+    end
   end
 
   def send_message(message)
