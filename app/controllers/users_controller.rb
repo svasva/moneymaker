8d69cb20e830f81648321ba1a8240a01d858c869
Vitaly A. Sorokin <erundook@gmail.com>
2012-11-20 14:55:31 +0400

    api refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,32 @@
 class UsersController < ApplicationController
   def send_message
-    @args = params[:args]
-    case params[:cmd]
-    when 'PING'
-      render json: 'pong'
-    when 'getItem'
-      @item = Item.find(@args.first)
-      render json: @item
-    when 'buyItem'
-      begin
+    begin
+      args = params[:args]
+      case params[:cmd]
+      when 'PING'
+        response = 'pong'
+      when 'getItem'
+        response = Item.find(args.first)
+      when 'buyItem'
         user = User.find(params[:id])
-        item_id, currency = @args
+        item_id, currency = args
         raise 'wrong arguments' unless item_id and currency
-        item = Item.find item_id
-        useritem = user.buy_item item, currency.to_sym
-        render json: useritem
-      rescue => e
-        render json: { error: e.inspect }
+        item = Item.find(item_id)
+        response = user.buy_item(item, currency.to_sym)
+      when 'sellItem'
+        item = UserItem.find(args.first).sell
+        raise 'item not sold' unless item
+        response = { success: 'item sold' }
+      when 'getItems'
+        response = Item.all
+      when 'startApplication'
+        response = { success: 'application started' }
+      else
+        raise 'command unknown'
       end
-    when 'sellItem'
-      begin
-        @item = UserItem.find(@args.first).sell
-        render json: { success: 'item sold' }
-      rescue => e
-        render json: { error: e.inspect }
-      end
-    when 'getItems'
-      @items = Item.all
-      render json: @items
-    when 'startApplication'
-      render json: {success: 'application started'}
-    else
-      render json: {error: 'command unknown'}
+      render json: response
+    rescue => e
+      render json: { error: e.inspect }
     end
   end
 end
