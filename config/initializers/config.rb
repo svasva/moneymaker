27ff2d9bd665c213438a6debb4ea48c9c921f818
Vitaly A. Sorokin <erundook@gmail.com>
2012-11-22 12:42:01 +0400

    SOCIAL_NETS selector

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -8,4 +8,4 @@ else
   SOCKET_URL = 'ws://192.168.1.242:9999/socket/0/0/websocket'
   CONTENT_URL = 'http://localhost:3000'
 end
-SOCIAL_NETS = %w(vkontakte moymir odnoklassniki facebook)
+SOCIAL_NETS = %w(vkontakte mailru odnoklassniki facebook)
