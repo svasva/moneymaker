290a3640ad0bc190dff939cae5c2890196b6f168
Vitaly A. Sorokin <erundook@gmail.com>
2012-12-03 11:33:26 +0400

    roomtype placement options fix

--- app/models/room_type.rb
+++ app/models/room_type.rb
@@ -1,4 +1,10 @@
 class RoomType < ItemType
   PLACEMENT_OPTIONS = %w(any ground underground onground)
   has_many :rooms
+  def self.placement_options
+    namespace = self.name.downcase
+    PLACEMENT_OPTIONS.map do |opt|
+      [I18n.t(namespace + '.placement_options.'+opt), opt]
+    end
+  end
 end
