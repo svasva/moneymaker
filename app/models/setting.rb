a4b558bff101a84cd7c02f0fb5e57ed371d41ff1
Vitaly A. Sorokin <erundook@gmail.com>
2012-12-22 17:26:33 +0400

    setting form & levels fix

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -4,6 +4,8 @@ class Setting
   field :start_coins, type: Integer, default: 100
   field :start_money, type: Integer, default: 10
 
+  validates_presence_of :time_per_client, :start_coins, :start_money
+
   def self.get
     self.find_or_create_by({})
   end
