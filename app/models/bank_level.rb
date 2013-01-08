33cd4cd7c9d4ce93419bc2ae08d6310cf3243526
Vitaly A. Sorokin <erundook@gmail.com>
2013-01-08 12:42:37 +0400

    level.rewards

--- app/models/bank_level.rb
+++ app/models/bank_level.rb
@@ -3,6 +3,7 @@ class BankLevel
   field :number,          type: Integer
   field :experience,      type: Integer
   field :min_reputation,  type: Integer
+  field :rewards,         type: Hash,    default: {}
 
   validates_presence_of :number, :experience, :min_reputation
   validates_uniqueness_of :number
