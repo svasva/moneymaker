cc455262b764027cf27b04e4c0f5ad7d208142b6
Vitaly A. Sorokin <erundook@gmail.com>
2013-01-11 19:52:29 +0400

    more fixing

--- app/models/flash_lib.rb
+++ app/models/flash_lib.rb
@@ -2,6 +2,8 @@ class FlashLib < Swfclient
   field :social,     type: String
   field :local_path, type: String
 
+  default_scope type(FlashLib)
+
   def path(local = false)
     local ? self.local_path : self.swf_url
   end
