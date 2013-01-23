5521e62e71d9b2c8f6f10e5528966ede40bc9a9b
Vitaly A. Sorokin <erundook@gmail.com>
2013-01-23 17:50:22 +0400

    fix rooms form

--- app/controllers/admin/branches_controller.rb
+++ app/controllers/admin/branches_controller.rb
@@ -5,4 +5,26 @@ class Admin::BranchesController < InheritedResources::Base
     @use_fields = %w(name desc level)
     true
   end
+
+  def edit
+    resource.requirements['items'] ||= {}
+    @req_items = resource.requirements['items'].map do |item_id, count|
+      { id: item_id,
+        name: Item.find(item_id).name,
+        count: count }
+    end
+    resource.requirements['rooms'] ||= {}
+    @req_rooms = resource.requirements['rooms'].map do |room_id, count|
+      { id: room_id,
+        name: Room.find(room_id).name,
+        count: count }
+    end
+    resource.effects ||= {}
+    @effects = resource.effects.map do |effect_id, count|
+      { id: effect_id,
+        name: I18n.t('item.effect_options.' + effect_id),
+        count: count }
+    end
+    super
+  end
 end
--- app/controllers/admin/rooms_controller.rb
+++ app/controllers/admin/rooms_controller.rb
@@ -5,6 +5,24 @@ class Admin::RoomsController < InheritedResources::Base
       [upg.name, upg.id]
     }
     @room_types = RoomType.all.map { |rt| [rt.name, rt.id] }
+    resource.requirements['items'] ||= {}
+    @req_items = resource.requirements['items'].map do |item_id, count|
+      { id: item_id,
+        name: Item.find(item_id).name,
+        count: count }
+    end
+    resource.requirements['rooms'] ||= {}
+    @req_rooms = resource.requirements['rooms'].map do |room_id, count|
+      { id: room_id,
+        name: Room.find(room_id).name,
+        count: count }
+    end
+    resource.effects ||= {}
+    @effects = resource.effects.map do |effect_id, count|
+      { id: effect_id,
+        name: I18n.t('item.effect_options.' + effect_id),
+        count: count }
+    end
     super
   end
 
