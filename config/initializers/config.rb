ae92b7564428d3ab73bdb3b1584526d5ddbdb372
Vitaly A. Sorokin <erundook@gmail.com>
2013-01-29 18:34:48 +0400

    rename app to mm

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -2,7 +2,7 @@ SOCIAL = YAML.load_file("#{Rails.root}/config/social.yml")[Rails.env]
 if Rails.env.production?
   SOCKET_API = 'http://ws.so14.org/sock'
   SOCKET_URL = 'ws://ws.so14.org/socket/websocket'
-  CONTENT_URL = 'http://app.so14.org'
+  CONTENT_URL = 'http://mm.so14.org'
 else
   SOCKET_API = 'http://127.0.0.1:9999/sock'
   SOCKET_URL = 'ws://127.0.0.1:9999/socket/websocket'
