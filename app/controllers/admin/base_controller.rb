8dc92b76d7d05a9fa6b0f92676912e08b964484a
Vitaly A. Sorokin <erundook@gmail.com>
2013-02-05 15:57:18 +0400

    refactor forms

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,13 @@
 class Admin::BaseController < InheritedResources::Base
   before_filter :authenticate_admin!
+  before_filter :set_rnames
 
   def show
     redirect_to collection_url
+  end
+
+  def set_rnames
+    @rpname = resource_class.name.pluralize.underscore
+    @rname = resource_class.name.pluralize.underscore
   end
 end
