329a454b1094bf7f190b1be2edcafa4f860c0aa0
Vitaly A. Sorokin <erundook@gmail.com>
2013-02-10 15:52:59 +0400

    fix after_update callback on User#give_rewards

--- app/models/user.rb
+++ app/models/user.rb
@@ -169,16 +169,17 @@ class User
           end
         end
       when 'experience'
-        self.inc :experience, rew.to_i
+        self.experience += rew.to_i
       when 'reputation'
-        self.inc :reputation, rew.to_i
+        self.reputation += rew.to_i
       # TODO: log financial stats
       when 'coins'
-        self.inc :coins, rew.to_i
+        self.coins += rew.to_i
       when 'money'
-        self.inc :money, rew.to_i
+        self.money += rew.to_i
       end
     end
+    self.save
   end
 
   def buy_content(content, currency)
